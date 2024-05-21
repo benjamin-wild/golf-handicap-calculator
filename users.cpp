6d2a0c0f8a9c12d592e7f01f25551b7e0eb1de73
Benjamin Wild <benwild771@gmail.com>
2024-05-20 22:55:14 -0400

    Beginning to add prompting functionality

--- users.cpp
+++ users.cpp
@@ -1,9 +1,24 @@
 #include "users.h"
 
-// main function that orchestrates calculating handicaps
-// for different users. Reads its variables from the input
-// file
-void Users::read_users(){
+/**
+ * Handling function which handles prompting the current user for the
+ * desired action they want to take - getting their handicap,
+ * calculating their handicap for a new course, or inputting their
+ * handicap score from a course previously played.
+ */
+void Users::prompt_user(){
+    while(true){
+        string action; 
+        std::cout << "Please select an action: Input Score | Calculate Handicap | Calculate Course Handicap\n"; 
+        std::cin >> action;
+    }
+}
+
+/**
+ * main function that orchestrates calculating handicaps for different
+ * users. Reads its variables from the input file
+ */
+void Users::read_score(){
     //read in user name and number of courses listed
     std::cin >> username >> num_courses; 
     string course_name; 
--- users.h
+++ users.h
@@ -8,12 +8,23 @@ class Users{
 private:
 	// relation from user to most recent courses played
 	unordered_map<string, deque<Course>> users;
+
+	// TODO - make a class/struct for specific user information/store in db
 	string username; 
 	int num_courses; 
 
 public:
-	//main function that orchestrates calculating handicaps 
-	//for different users. Reads its variables from the input
-	//file 
-	void read_users(); 
+	/**
+	 * Handling function which handles prompting the current user for the 
+	 * desired action they want to take - getting their handicap, 
+	 * calculating their handicap for a new course, or inputting their 
+	 * handicap score from a course previously played. 
+	*/
+	void prompt_user();
+
+	/**
+	 * main function that orchestrates calculating handicaps for different 
+	 * users. Reads its variables from the input file
+	*/
+	void read_score(); 
 };
