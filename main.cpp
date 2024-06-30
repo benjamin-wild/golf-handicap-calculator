8573aec62405badc84770e6b88531eacf38f8cc2
Benjamin Wild <benwild771@gmail.com>
2024-06-29 22:29:35 -0400

    db query for inserting a score

--- main.cpp
+++ main.cpp
@@ -8,6 +8,19 @@
 using std::cout; 
 using std::string;
 
+// Function used to print the data base
+static int callback(void *data, int argc, char **argv, char **azColName){
+    int i;
+    fprintf(stderr, "%s: ", (const char *)data);
+
+    for (i = 0; i < argc; i++){
+        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+    }
+
+    printf("\n");
+    return 0;
+}
+
 /**
  * main function that orchestrates calculating handicaps for different
  * users. Reads its variables from the input file
@@ -32,6 +45,7 @@ std::unique_ptr<DB_Entry> read_score(){
     }
     catch (std::exception &e){
         throw e.what();
+        exit(1);
     }
 
     // Store this in an object and return
@@ -55,8 +69,37 @@ void calcualte_differential(){
  * persistent storage
  */
 void input_scores(){
-    // attempt to connect to the database
-    // input score into scores table if possible
+    // Connect to Database
+    sqlite3 *db;
+    char *zErrMsg = nullptr;
+    int rc = 0;
+
+    // Open database
+    rc = sqlite3_open("var/handicap-calculator.sqlite3", &db);
+
+    // Check to ensure database opened correctly
+    if (rc){
+        std::cerr << "Can't open database: " << sqlite3_errmsg(db) << "\n";
+        return;
+    } else {
+        cout << "Opened database successfully\n";
+
+        // Run query here
+        string testing_q = "SELECT * FROM users;";
+        cout << "Current State of Table 'users'\n";
+        // Table should be populated already
+        try{
+            if (sqlite3_exec(db, testing_q.c_str(), callback, NULL, NULL)){
+                throw std::runtime_error(std::string("Failed query ") + testing_q);
+            }
+        }
+        catch (const std::exception &e){
+            cout << e.what() << "\n";
+            exit(1);
+        }
+
+        sqlite3_close(db);
+    }
 }
 
 /**
@@ -65,23 +108,10 @@ void input_scores(){
 void retrieve_handicap(){
     while(true){
         // Call to read the score inputted
-        read_score();
+        std::unique_ptr<DB_Entry> entry = read_score();
         input_scores(); 
     }
     calcualte_differential(); 
-}
-
-// Function used to print the data base
-static int callback(void *data, int argc, char **argv, char **azColName){
-    int i;
-    fprintf(stderr, "%s: ", (const char *)data);
-
-    for (i = 0; i < argc; i++){
-        printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-    }
-
-    printf("\n");
-    return 0;
 }
 
 /*
@@ -90,8 +120,7 @@ static int callback(void *data, int argc, char **argv, char **azColName){
     this application allows for that.
 */
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     // Determine Help functionality or Action
     // Help Menu
     if (argc != 1){
@@ -102,38 +131,5 @@ int main(int argc, char *argv[])
             "store thier data for easier use later on.";
         exit(1);
     }
-    // Connect to Database
-    sqlite3 *db;
-    char *zErrMsg = nullptr;
-    int rc = 0;
-
-    // Open database
-    rc = sqlite3_open("var/handicap-calculator.sqlite3", &db);
-
-    // Check to ensure database opened correctly
-    if (rc) {
-        std::cerr << "Can't open database: " << sqlite3_errmsg(db) << "\n";
-        return 0;
-    } else {
-        cout << "Opened database successfully\n";
-
-        // Testing query
-        string testing_q = "SELECT * FROM users;"; 
-        cout << "Current State of Table 'users'\n"; 
-        // Table should be populated already
-        try{
-            if(sqlite3_exec(db, testing_q.c_str(), callback, NULL, NULL)){
-                throw std::runtime_error(std::string("Failed query ") + testing_q); 
-            } 
-        }
-        catch(const std::exception &e) {
-            cout << e.what() << "\n"; 
-            exit(1); 
-        }
-        /*  Create thread to handle user input/program outputting, 
-            thread to handle database accesses, and thread to handle
-            any necessary computations. (not sure about the need of the last one) */
-
-        sqlite3_close(db); 
-    }
+    retrieve_handicap(); 
 }
